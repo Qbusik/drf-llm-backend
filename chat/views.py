ed48c3e90aedeb1e6f1c2ff3cd9b3393789dabc5
Jakub Paluch <qbusik@gmail.com>
2026-05-15 11:16:25 +0200

    Add chat views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import PromptJob
+from .serializers import PromptSerializer
+from .tasks import process_prompt
+
+
+class PromptView(APIView):
+
+    def post(self, request):
+
+        serializer = PromptSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        prompt = serializer.validated_data["prompt"]
+
+        job = PromptJob.objects.create(prompt=prompt)
+
+        process_prompt.delay(str(job.id))
+
+        return Response({"job_id": job.id, "status": job.status})
+
+
+class PromptStatusView(APIView):
+
+    def get(self, request, job_id):
+
+        job = PromptJob.objects.get(id=job_id)
+
+        return Response(
+            {
+                "id": job.id,
+                "status": job.status,
+                "response": job.response,
+                "error": job.error,
+            }
+        )
