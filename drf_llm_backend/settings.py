f116b01d2e421d49772431908f49e06b1819d3cb
Jakub Paluch <qbusik@gmail.com>
2026-05-16 11:26:06 +0200

    Set for AWS deploy

--- drf_llm_backend/settings.py
+++ drf_llm_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
